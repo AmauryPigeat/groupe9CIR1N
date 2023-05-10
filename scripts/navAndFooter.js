ce49f0dc9d8c25a42fdf6df0d224c6de3a9ebffc
AmauryPigeat <amaury.pigeat@isen-ouest.yncrea.fr>
2023-05-10 15:23:04 +0200

    Link du logo vers accueil

---
+++ scripts/navAndFooter.js
@@ -0,0 +1,4 @@
+function mainLogoLink(){ 
+    //permet de rediriger vers accueil sans passer par une balise <a> à cause du style qui fait qu'on doit tout modifier (gain de temps)
+    window.open("../accueil.html","_blank");
+}
